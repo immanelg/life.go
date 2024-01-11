d3a3e6bb771e3c013b01029ca6941b437b8a9efb
immanelg <119798691+immanelg@users.noreply.github.com>
2024-01-11 14:46:35 +0300

    Event loop

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	// "fmt"
 	"log"
-	// "os"
+	"math/rand"
+	"time"
 
 	"github.com/gdamore/tcell"
 )
@@ -15,11 +16,11 @@ type Cell struct {
 }
 
 type Matrix struct {
-	cells      []Cell
-	w, h int
+	cells []Cell
+	w, h  int
 }
 
-func newMatrix(w, h int) Matrix {
+func NewMatrix(w, h int) Matrix {
 	m := Matrix{cells: []Cell{}, w: w, h: h}
 	for i := 0; i < w; i++ {
 		for j := 0; j < h; j++ {
@@ -29,91 +30,105 @@ func newMatrix(w, h int) Matrix {
 	return m
 }
 
-func (m *Matrix) get(x, y int) (cell Cell, ok bool) {
-	if x >= m.w || x < 0 || y >= m.h || y < 0 {
-		return
-	}
-	return m.cells[x*m.h+y], true
+func (m *Matrix) exists(x, y int) bool {
+	return x < m.w && x >= 0 && y < m.h && y >= 0
 }
 
-func (m *Matrix) getNeighbours(x, y int) (cells []Cell) {
-	if _, exists := m.get(x, y); !exists {
-		log.Panicf("should exist: (%d, %d)", x, y)
+func (m *Matrix) get(x, y int) *Cell {
+	if !m.exists(x, y) {
+		log.Panicf("get: key does not exist: (%d, %d)", x, y)
 	}
-	for a := range []int{-1, 0, +1} {
-		for b := range []int{-1, 0, +1} {
+	return &m.cells[x*m.h+y]
+}
+
+func (m *Matrix) Live(x, y int) {
+	m.get(x, y).alive = true
+}
+
+func (m *Matrix) neighbours(x, y int) (alive int) {
+	if !m.exists(x, y) {
+		log.Panicf("neighbours: key does not exist: (%d, %d)", x, y)
+	}
+	for a := -1; a <= 1; a++ {
+		for b := -1; b <= 1; b++ {
 			if a == 0 && b == 0 {
 				continue
 			}
-			cell, exists := m.get(x+a, y+b)
-			if !exists {
-				continue
+			if m.exists(x+a, y+b) && m.get(x+a, y+b).alive {
+				alive++
 			}
-			cells = append(cells, cell)
 		}
 	}
 	return
 }
 
-func (m *Matrix) tick(x, y int) {
-	matrixAfter := newMatrix(m.w, m.h)
-	for _, cell := range m.cells {
-		neighbours := m.getNeighbours(cell.x, cell.y)
-		aliveNeighbours := 0
-		deadNeighbours := 0
-		for _, n := range neighbours {
-			if n.alive {
-				aliveNeighbours++
-			} else {
-				deadNeighbours++
-			}
-		}
+func (m *Matrix) Randomize() {
+	for i := range m.cells {
+		m.cells[i].alive = rand.Intn(8) == 1 
+	}
+}
+
+func (m *Matrix) Tick() {
+	newCells := make([]Cell, len(m.cells))
+
+	for i, cell := range m.cells {
+		c := cell
+		neighbours := m.neighbours(cell.x, cell.y)
+
 		// Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 		// Any live cell with two or three live neighbours lives on to the next generation.
 		// Any live cell with more than three live neighbours dies, as if by overpopulation.
 		// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
-		if aliveNeighbours < 2 {
-			cell.alive = false
+		if cell.alive {
+			c.alive = neighbours == 2 || neighbours == 3
+		} else {
+			c.alive = neighbours == 3
 		}
-		if aliveNeighbours == 2 || aliveNeighbours == 3 {
-			cell.alive = true
-		}
-		matrixAfter.cells = append(matrixAfter.cells, cell)
+		newCells[i] = c
+	}
+	for i, c := range newCells {
+		m.cells[i] = c
 	}
 }
-
-var TermH, TermW int
 
 func main() {
 	screen, _ := tcell.NewScreen()
 	screen.Init()
 	screen.Clear()
 
-	TermW, TermH = screen.Size()
+	TermW, TermH := screen.Size()
 
-loop:
+	matrix := NewMatrix(TermW, TermH)
+	matrix.Randomize()
+
+	events := make(chan tcell.Event)
+
+	go func() {
+		for {
+			events <- screen.PollEvent()
+		}
+	}()
+
+eventloop:
 	for {
-		screen.SetContent(0, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(1, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(2, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(3, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(4, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(5, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(6, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(7, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(8, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(9, 0, '1', nil, tcell.StyleDefault)
-		screen.SetContent(10, 0, '1', nil, tcell.StyleDefault)
-
-		screen.Show()
-
-		ev := screen.PollEvent()
-		switch ev := ev.(type) {
-		case *tcell.EventKey:
-			if ev.Key() == tcell.KeyCtrlC {
-				break loop
+		select {
+		case ev := <-events:
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyCtrlC || ev.Key() == tcell.KeyEsc {
+					break eventloop
+				}
 			}
+		case <-time.After(time.Duration(time.Second/2)):
+			for _, c := range matrix.cells {
+				if c.alive {
+					screen.SetContent(c.x, c.y, '@', nil, tcell.StyleDefault)
+				} else {
+					screen.SetContent(c.x, c.y, ' ', nil, tcell.StyleDefault)
+				}
+			}
+			matrix.Tick()
+			screen.Show()
 		}
 	}
 	screen.Fini()
